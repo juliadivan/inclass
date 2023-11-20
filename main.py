578f9d1a5c4068087269eda5519d23e7f2630974
Jesse Gilbert <jessegilbert6902@gmail.com>
2023-11-20 09:48:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 def game(number):
     if number == 1:
         print ("Hooray!")
     else:
         print("Wrong")
+=======
+
+#testing 123
+'''
+sdfsdlkj aslkdfjs;df
+asdfad'[osfja
+sd[pfos
+GKJAKSFDJG
+PFSOJG
+PSD]]'''
+>>>>>>> Stashed changes
