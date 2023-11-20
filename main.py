40afd77bef8b98130573e1ea044e59eaa18b7d7d
houlton888 <houlton888@gmail.com>
2023-11-20 09:46:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import paho.mqtt.client as mqtt
+broker_adress = "broker.hivemq.com"
+client = mqtt.Client("team!")
+client.connect(broker_adress)
+print('Connected to %s MQTT broker' % broker_adress)
+client.subscribe("ME35")
+
+# client.publish("ME35","Hi")
+
 
